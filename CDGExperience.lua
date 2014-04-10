d762a3f1709beefce37d3b4581cc18cd856d01f2
simenon <albert@simenon.nl>
2014-04-10 21:55:19 +0200

    modified:   CDGExperience.lua

--- CDGExperience.lua
+++ CDGExperience.lua
@@ -1,3 +1,30 @@
+XP_REASON_ACTION
+XP_REASON_BATTLEGROUND
+XP_REASON_COLLECT_BOOK
+XP_REASON_COMMAND
+XP_REASON_COMPLETE_POI
+XP_REASON_DISCOVER_POI
+XP_REASON_FINESSE
+XP_REASON_KEEP_REWARD
+XP_REASON_KILL
+XP_REASON_LOCK_PICK
+XP_REASON_MEDAL
+XP_REASON_NONE
+XP_REASON_OVERLAND_BOSS_KILL
+XP_REASON_QUEST
+XP_REASON_REWARD
+XP_REASON_SCRIPTED_EVENT
+XP_REASON_SKILL_AVA
+XP_REASON_SKILL_BOOK
+XP_REASON_SKILL_GUILD_REP
+XP_REASON_SKILL_TRADESKILL
+XP_REASON_SKILL_TRADESKILL_ACHIEVEMENT
+XP_REASON_SKILL_TRADESKILL_CONSUME
+XP_REASON_SKILL_TRADESKILL_HARVEST
+XP_REASON_SKILL_TRADESKILL_QUEST
+XP_REASON_SKILL_TRADESKILL_RECIPE
+XP_REASON_SKILL_TRADESKILL_TRAIT
+
 function CDGExperience_OnInitialized()
 	--	EVENT_QUEST_COMPLETE_EXPERIENCE (string questName, integer xpGained)
 	--	EVENT_EXPERIENCE_GAIN (integer value, integer reason)
@@ -6,10 +33,43 @@ function CDGExperience_OnInitialized()
 	EVENT_MANAGER:RegisterForEvent("CDGExperience",EVENT_EXPERIENCE_GAIN_DISCOVERY,CDGExperience_ExperienceGainDiscovery )
 end
 
-function CDGExperience_ExperienceGain(value, reason)
-	d(string.format("Gained %d XP from %d",value, reason))
+function XPReasonToString(reason)
+	local sReason = "UNKNOWN"
+	if     reason == XP_REASON_ACTION then sReason = "XP_REASON_ACTION"
+	elseif reason == XP_REASON_BATTLEGROUND then sReason = "XP_REASON_BATTLEGROUND"
+	elseif reason == XP_REASON_COLLECT_BOOK then sReason = "XP_REASON_COLLECT_BOOK"
+	elseif reason == XP_REASON_COMMAND then sReason = "XP_REASON_COMMAND"
+	elseif reason == XP_REASON_COMPLETE_POI then sReason = "XP_REASON_COMPLETE_POI"
+	elseif reason == XP_REASON_DISCOVER_POI then sReason = "XP_REASON_DISCOVER_POI"
+	elseif reason == XP_REASON_FINESSE then sReason = "XP_REASON_FINESSE"
+	elseif reason == XP_REASON_KEEP_REWARD then sReason = "XP_REASON_KEEP"
+	elseif reason == XP_REASON_KILL then sReason = "XP_REASON_KILL"
+	elseif reason == XP_REASON_LOCK_PICK then sReason = "XP_REASON_LOCK_PICK"
+	elseif reason == XP_REASON_MEDAL then sReason = "XP_REASON_MEDAL"
+	elseif reason == XP_REASON_NONE then sReason = "XP_REASON_NONE"
+	elseif reason == XP_REASON_OVERLAND_BOSS_KILL then sReason = "XP_REASON_OVERLAND_BOSS_KILL"
+	elseif reason == XP_REASON_QUEST then sReason = "XP_REASON_QUEST"
+	elseif reason == XP_REASON_REWARD then sReason = "XP_REASON_REWARD"
+	elseif reason == XP_REASON_SCRIPTED_EVENT then sReason = "XP_REASON_SCRIPTED_EVENT"
+	elseif reason == XP_REASON_SKILL_AVA then sReason = "XP_REASON_SKILL_AVA"
+	elseif reason == XP_REASON_SKILL_BOOK then sReason = "XP_REASON_SKILL_BOOK"
+	elseif reason == XP_REASON_SKILL_GUILD_REP then sReason = "XP_REASON_SKILL_GUILD_REP"
+	elseif reason == XP_REASON_SKILL_TRADESKILL then sReason = "XP_REASON_SKILL_TRADESKILL"
+	elseif reason == XP_REASON_SKILL_TRADESKILL_ACHIEVEMENT then sReason = "XP_REASON_SKILL_TRADESKILL_ACHIEVEMENT"
+	elseif reason == XP_REASON_SKILL_TRADESKILL_CONSUME then sReason = "XP_REASON_SKILL_TRADESKILL_CONSUME"
+	elseif reason == XP_REASON_SKILL_TRADESKILL_HARVEST then sReason = "XP_REASON_SKILL_TRADESKILL_HARVEST"
+	elseif reason == XP_REASON_SKILL_TRADESKILL_QUEST then sReason = "XP_REASON_SKILL_TRADESKILL_QUEST"
+	elseif reason == XP_REASON_SKILL_TRADESKILL_RECIPE then sReason = "XP_REASON_SKILL_TRADESKILL_RECIPE"
+	elseif reason == XP_REASON_SKILL_TRADESKILL_TRAIT then sReason = "XP_REASON_SKILL_TRADESKILL_TRAIT"
+	end
+	return sReason
 end
 
-function CDGExperience_ExperienceGainDiscovery(value, areaName)
+function CDGExperience_ExperienceGain(value, reason)
+	
+	d(string.format("Gained %d XP from %s",value, XPReasonToString(reason)))
+end
+
+function CDGExperience_ExperienceGainDiscovery(areaName, value)
 	d(string.format("Gained %d Discover XP from %s",value, areaName))
 end
